d6e7ca11c45dd5565c5afaeaf0a2e0064462f9ea
Lucas Machado <lucasbermac@gmail.com>
2023-04-10 22:00:33 -0300

    Adicionado: Cadastro e integração com o mysql

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+from mysql.connector.errors import IntegrityError
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -6,9 +9,53 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html')
+def create_conn():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="db_pi"
+    )
+    return conn
 
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        fullname = request.form['DC_FULLNAME']
+        birthdate = request.form['DT_BIRTHDATE']
+        birthdate = datetime.strptime(birthdate, "%d/%m/%Y").strftime("%Y-%m-%d")
+        document = request.form['DC_DOCUMENT']
+        email = request.form['DC_MAIL']
+        phone = request.form['DC_PHONE']
+        
+        conn = create_conn()
+        cursor = conn.cursor()
+        
+        try:
+            sql_query = "INSERT INTO TB_CLIENTS (DC_FULLNAME, DT_BIRTHDATE, DC_DOCUMENT, DC_MAIL, DC_PHONE) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql_query, (fullname, birthdate, document, email, phone))
+            conn.commit()
+            msg = "Usuário cadastrado com sucesso!"
+        except IntegrityError as err:
+            if 'DC_DOCUMENT' in str(err):
+                msg = "CPF já cadastrado."
+            elif 'DC_MAIL' in str(err):
+                msg = "E-mail já cadastrado."
+            else:
+                msg = "Erro ao cadastrar usuário."
+        except mysql.connector.Error as err:
+            print("Erro ao inserir usuário:", err)
+            msg = "Erro ao cadastrar usuário."
+            raise
+        finally:
+            cursor.close()
+            conn.close()
+
+        return redirect(url_for('cadastro', msg=msg))
+    
+    else:
+        msg = request.args.get('msg')
+        return render_template('cadastro.html', msg=msg)
+    
 if __name__ == '__main__':
     app.run(debug=True, port=5000)

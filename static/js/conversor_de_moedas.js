250812f7927b9cca62c192af9b83a8662df7c7d5
Salazar-ata <79613364+Salazar-ata@users.noreply.github.com>
2023-10-16 20:15:22 -0300

    .

---
+++ static/js/conversor_de_moedas.js
@@ -0,0 +1,21 @@
+let quantia = document.querySelector(".quantia");
+let botaoConverter = document.querySelector("button");
+let resultado = document.querySelector(".resultado");
+
+async function converterMoeda(){
+    let requestURL = `https://economia.awesomeapi.com.br/last/USD-BRL`;
+
+    try {
+        let response = await fetch(requestURL);
+        let data = await response.json();
+        resultado.textContent = Number(data.USDBRL.high).toFixed(2)*Number(quantia.value);
+      } catch (error) {
+        console.log(error);
+      }
+      
+}
+
+botaoConverter.addEventListener("click", (e) => {
+    e.preventDefault();
+    converterMoeda();
+});

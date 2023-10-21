211c915102fa5944a20f7e2db08237bef119091c
Lucas Machado <lucasbermac@gmail.com>
2023-10-21 11:46:42 -0300

    feature: adicionando rota de cadastro

--- app.py
+++ app.py
@@ -20,6 +20,45 @@ def catalogo():
         else:
             return "Id invalido"
 
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        fullname = request.form['DC_FULLNAME']
+        birthdate = request.form['DT_BIRTHDATE']
+        birthdate = datetime.strptime(birthdate, "%d/%m/%Y").strftime("%Y-%m-%d")
+        document = request.form['DC_DOCUMENT']
+        email = request.form['DC_MAIL']
+        phone = request.form['DC_PHONE']
+        
+        conn = create_conn()
+        cursor = conn.cursor()
+        
+        try:
+            sql_query = "INSERT INTO TB_CLIENTS (DC_FULLNAME, DT_BIRTHDATE, DC_DOCUMENT, DC_MAIL, DC_PHONE) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql_query, (fullname, birthdate, document, email, phone))
+            conn.commit()
+            msg = "Usuário cadastrado com sucesso!"
+        except IntegrityError as err:
+            if 'DC_DOCUMENT' in str(err):
+                msg = "CPF já cadastrado."
+            elif 'DC_MAIL' in str(err):
+                msg = "E-mail já cadastrado."
+            else:
+                msg = "Erro ao cadastrar usuário."
+        except mysql.connector.Error as err:
+            print("Erro ao inserir usuário:", err)
+            msg = "Erro ao cadastrar usuário."
+            raise
+        finally:
+            cursor.close()
+            conn.close()
+
+        return redirect(url_for('cadastro', msg=msg))
+    
+    else:
+        msg = request.args.get('msg')
+        return render_template('cadastro.html', msg=msg)     
+
 @app.route('/efetiva_compra', methods=['GET', 'POST'])
 def efetiva_compra():
         if request.method == 'GET':

cf543d9d377dee4a5a2d50ade9ac32632d807a21
Lucas Machado <lucasbermac@gmail.com>
2023-10-21 10:46:03 -0300

    feature: Adicionando rota localhost para ser acessível externamente

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def conversor():
         return render_template('conversor_de_moedas.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
